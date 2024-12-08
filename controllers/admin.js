1d25d874d60dcaee47e4c2862a0c73879b4ba712
Dddxx98 <deexith2016@gmail.com>
2024-12-08 12:12:10 +0530

    sixth

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,11 +1,13 @@
 const path = require('path');
 const rootDir = require('../util/path');
+const Product = require('../models/product');
 
 module.exports.getProductController = (req, res) => {
     res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 }
 
 module.exports.postProductController = (req, res) => {
-    console.log(req.body);
+    const product = new Product(req.body.title);
+    product.save();
     res.redirect('/');
 }
