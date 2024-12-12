16b3c0153d1ac88046f61df0517e258f556e082a
Dddxx98 <deexith2016@gmail.com>
2024-12-12 10:26:56 +0530

    twelth

--- controllers/admin.js
+++ controllers/admin.js
@@ -8,10 +8,7 @@ module.exports.getProductController = (req, res) => {
 
 module.exports.postProductController = (req, res) => {
     const price = Math.random() * 100;
-    const product = new Product(null,req.body.title, price);
-    product.save().then(() => {
-        res.redirect('/');
-    }).catch(err => console.log(err));
+    req.user.createProduct({ title: req.body.title, price: price }).then(ans => res.redirect('/')).catch(err => console.log(err))
 }
 
 module.exports.getProductWithID = (req, res) => {
@@ -20,5 +17,63 @@ module.exports.getProductWithID = (req, res) => {
     Product.findById(prodId).then((product) => {
         console.log(product)
         res.redirect('/no');
-    }).catch(err=>console.log(err));
+    }).catch(err => console.log(err));
 }
+
+exports.getEditProduct = (req, res, next) => {
+    const editMode = req.query.edit;
+    if (!editMode) {
+        return res.redirect('/');
+    }
+    const prodId = req.params.productId;
+    req.user
+        .getProducts({ where: { id: prodId } })
+        // Product.findById(prodId)
+        .then(products => {
+            const product = products[0];
+            if (!product) {
+                return res.redirect('/');
+            }
+            res.render('admin/edit-product', {
+                pageTitle: 'Edit Product',
+                path: '/admin/edit-product',
+                editing: editMode,
+                product: product
+            });
+        })
+        .catch(err => console.log(err));
+};
+
+exports.postEditProduct = (req, res, next) => {
+    const prodId = req.body.productId;
+    const updatedTitle = req.body.title;
+    const updatedPrice = req.body.price;
+    const updatedImageUrl = req.body.imageUrl;
+    const updatedDesc = req.body.description;
+    Product.findById(prodId)
+        .then(product => {
+            product.title = updatedTitle;
+            product.price = updatedPrice;
+            product.description = updatedDesc;
+            product.imageUrl = updatedImageUrl;
+            return product.save();
+        })
+        .then(result => {
+            console.log('UPDATED PRODUCT!');
+            res.redirect('/admin/products');
+        })
+        .catch(err => console.log(err));
+};
+
+exports.getProducts = (req, res, next) => {
+    req.user
+        .getProducts()
+        .then(products => {
+            res.render('admin/products', {
+                prods: products,
+                pageTitle: 'Admin Products',
+                path: '/admin/products'
+            });
+        })
+        .catch(err => console.log(err));
+};
